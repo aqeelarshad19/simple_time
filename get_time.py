2c3fc6d909480fcb1b40e30b9d630d1cf0be63e7
Muhammad Aqeel Arshad <m.arshad@osram.com>
2019-10-14 14:36:15 +0200

    Added fucntion to get time in newyork

--- get_time.py
+++ get_time.py
@@ -1,3 +1,17 @@
+import datetime, pytz
 from time import gmtime, strftime
-t = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-print t
+
+def get_nyc_time():
+    utc = pytz.utc
+    utc_dt = datetime.datetime.now(pytz.timezone('US/Eastern'))
+    eastern = pytz.timezone('US/Eastern')
+    loc_dt = utc_dt.astimezone(eastern)
+    fmt = '%Y-%m-%d %H:%M:%S %Z%z'
+    loc_dt.strftime(fmt)
+    return loc_dt
+
+if __name__ == "__main__":
+    local_time=strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    print("Local Time: {}".format(local_time))
+    print("Time in Newyork: {}".format(get_nyc_time()))
+
